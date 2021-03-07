c1b3536627c2fa802e872a43b9060f1bb8ed092b
alexseveneight <alexseveneight@gmail.com>
2021-03-07 09:22:41 +0300

    'api'

---
+++ src/services/swapi-service.js
@@ -0,0 +1,57 @@
+//const fetch = require('node-fetch');
+
+export default class SwapiService {
+
+  _apiBase = 'https://swapi.dev/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, received ${res.status}`)
+    }
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  } 
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results.map(this._transformPlanet);
+  }
+
+  async getPlanet(id) {
+    let planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet)
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
+
+  _transformPlanet(planet){
+    const idRegExp = /\/([0-9]*)\/$/;
+    const id = planet.url.match(idRegExp)[1];
+
+    return {
+      id, 
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    }
+  }
+}
